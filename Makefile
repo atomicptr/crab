450c71d94d713f79b8dd91abf755923f3de64082
Christopher Kaster <me@atomicptr.de>
2020-03-27 13:31:29 +0100

    add Makefile to properly build release

---
+++ Makefile
@@ -0,0 +1,14 @@
+.PHONY = build
+
+meta_path = github.com/atomicptr/crab/pkg/meta
+git_commit := $(shell git rev-list -1 HEAD)
+git_version := $(shell git --no-pager tag --points-at HEAD | head -n 1)
+
+build:
+	go build -o bin/crab \
+		-ldflags "\
+			-X $(meta_path).Version=$(git_version) \
+			-X $(meta_path).GitCommit=$(git_commit)" \
+		cmd/crab/main.go
+test:
+	go test -v ./...
--- pkg/meta/version.go
+++ pkg/meta/version.go
@@ -2,7 +2,7 @@ package meta
 
 import "fmt"
 
-var Version = "dev"
+var Version = ""
 var GitCommit = ""
 var UserAgent = "github.com/atomicptr/crab"
 
@@ -10,7 +10,11 @@ func VersionString() string {
 	commitString := ""
 	// ignore warning, this value will be later added as a build flag
 	if len(GitCommit) >= 7 {
-		commitString = fmt.Sprintf(" (%s)", GitCommit[:7])
+		commitString = fmt.Sprintf("-%s", GitCommit[:7])
+	}
+
+	if Version == "" {
+		Version = "dev"
 	}
 
 	return fmt.Sprintf("%s%s", Version, commitString)

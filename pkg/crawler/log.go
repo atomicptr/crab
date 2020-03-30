45a512534708ce5625479ccc4143efd580c5a4bb
Christopher Kaster <me@atomicptr.de>
2020-03-30 15:13:15 +0200

    mutex lock printing to stdout, fix bug with unescaped error messages

--- pkg/crawler/log.go
+++ pkg/crawler/log.go
@@ -1,10 +1,23 @@
 package crawler
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"sync"
 	"time"
 )
 
+var printMutex sync.Mutex
+
+// safePrintln logs a message protected by a mutex
+func safePrintln(message string) {
+	printMutex.Lock()
+	fmt.Println(message)
+	printMutex.Unlock()
+}
+
+// log logs a json log with the status code, url, timestamp and duration of the request
 func log(statusCode int, url string, duration time.Duration) {
 	message := fmt.Sprintf(
 		`{"status": %d, "url": "%s", "time": %d, "duration": %d}`,
@@ -13,16 +26,28 @@ func log(statusCode int, url string, duration time.Duration) {
 		time.Now().Unix(),
 		duration.Milliseconds(),
 	)
-	fmt.Println(message)
+	safePrintln(message)
 }
 
+// logError logs a json log with an error, url, timestamp and duration of the request
 func logError(err error, url string, duration time.Duration) {
 	message := fmt.Sprintf(
-		`{"err": %s, "url": "%s", "time": %d, "duration": %d}\n`,
-		err,
+		`{"err": %s, "url": "%s", "time": %d, "duration": %d}`,
+		escapeString(err.Error()),
 		url,
 		time.Now().Unix(),
 		duration.Milliseconds(),
 	)
-	fmt.Println(message)
+	safePrintln(message)
+}
+
+// escapeString escapes a string to be used as a json value
+func escapeString(str string) string {
+	b, err := json.Marshal(str)
+	if err != nil {
+		// could not parse it, base64 encode it and send that into the log instead
+		bStr := base64.StdEncoding.EncodeToString([]byte(str))
+		return "base64:" + bStr
+	}
+	return string(b)
 }
